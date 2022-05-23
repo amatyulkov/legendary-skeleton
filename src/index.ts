4c15aedca35ea1e221a2269ee56b2e331716e74a
Anton Matyulkov <anton@matyulkov.me>
2022-05-23 22:02:50 +0400

    Add TODOs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 // TODO: fix script path on github pages
 // TODO: custom identity
 // TODO: custom attributes on skeleton parts
+// TODO: custom part configs: opacity, texture? etc
 // TODO: control skeleton colors
 // TODO: control skeleton angle
 // TODO: control skeleton duration
-// TODO: custom part configs: opacity, texture? etc
 // TODO: use existing SVG as mask
 // TODO: animate in-out
-
+// TODO: infer skeleton color based on current color
+// TODO: allow to layer skeletons
 import { Skeleton } from "./components/skeleton";
 import { SkeletonPart } from "./components/skeleton-part";
 
